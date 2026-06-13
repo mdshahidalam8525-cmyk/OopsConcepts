a6de1ec55c2b8d03641d2568184529f0603895aa
Shahid <mdshahidalam8525@gmail.com>
2026-06-14 01:49:38 +0530

    Add some changes

--- ComplexNoclassCode.py
+++ ComplexNoclassCode.py
@@ -1,1 +1,17 @@
-print('Hallo')
+class ComplexNumber:
+    def __init__(self, real=0.0,imag=0.0):
+        self.imag = imag
+        self.real = real
+
+    def __str__(self):
+        str = f"{self.real} + {self.imag}i"
+        return str
+
+    def Conjugate(self):
+        imag  = self.imag* -1
+        print(f"({self.real} + {imag}i)")
+
+ComNum = ComplexNumber(3,5)
+ComNum1 = ComNum.Conjugate()
+print((ComNum.real,ComNum.imag))
+print(ComNum1)
--- oopsExample.py
+++ oopsExample.py
@@ -10,5 +10,16 @@
 
 #! Let me knoe complex number class
 
+c1 = 3+4j
+c2 = 4+5j
+print(type(c1))
+print(type(c2))
+print(c1+c2)
+print(c1-c2)
+print(c1*c2)
+print(c1/c2)
 
-print('hallo')
+c = complex()
+print(c)
+print(c1.conjugate())
+print(c1)
